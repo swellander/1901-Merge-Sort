e889ce542fdb3ed2e55a1a1cae467329a7bf12fb
Eliot Szwajkowski <eliotpszw@gmail.com>
2019-01-17 20:26:27 -0500

    README added.

---
+++ README.md
@@ -0,0 +1,3 @@
+# MergeSort
+
+Video can be found [here!](https://youtu.be/a2wij0F2dxw)
--- js/index.js
+++ js/index.js
@@ -1,30 +1,20 @@
-type Merge = (left: number[], right: number[]) => number[];
-const merge: Merge = (left, right) => {
+const merge = (left, right) => {
   const mergedNumbers = [];
-
   while (left.length && right.length) {
     if (left[0] <= right[0]) mergedNumbers.push(left.shift());
     else mergedNumbers.push(right.shift());
   }
-
   const mergedResult = mergedNumbers.concat(left).concat(right);
-
   return mergedResult;
 };
-
-type Split = (numbers: number[]) => { left: number[]; right: number[] };
-const split: Split = numbers => ({
+const split = numbers => ({
   left: numbers.slice(0, Math.floor(numbers.length / 2)),
   right: numbers.slice(Math.floor(numbers.length / 2)),
 });
-
-type MergeSort = (numbers: number[]) => number[];
-const mergeSort: MergeSort = (numbers = []) => {
+const mergeSort = (numbers = []) => {
   if (numbers.length <= 1) return numbers;
-
   const { left, right } = split(numbers);
-
   return merge(mergeSort(left), mergeSort(right));
 };
-
-export = mergeSort;
+module.exports = mergeSort;
+//# sourceMappingURL=index.js.map
