b0c16f1115049441bf06722d47855122c09ef947
Eliot Szwajkowski <eliotpszw@gmail.com>
2019-01-17 19:53:53 -0500

    Solution.

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+type Merge = (left: number[], right: number[]) => number[];
+const merge: Merge = (left, right) => {
+  const mergedNumbers = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) mergedNumbers.push(left.shift());
+    else mergedNumbers.push(right.shift());
+  }
+
+  const mergedResult = mergedNumbers.concat(left).concat(right);
+
+  return mergedResult;
+};
+
+type Split = (numbers: number[]) => { left: number[], right: number[] };
+const split: Split = numbers => ({
+  left: numbers.slice(0, Math.floor(numbers.length / 2)),
+  right: numbers.slice(Math.floor(numbers.length / 2)),
+});
+
+type MergeSort = (numbers: number[]) => number[];
+const mergeSort: MergeSort = (numbers = []) => {
+  if (numbers.length <= 1) return numbers;
+
+  const { left, right } = split(numbers);
+
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+export = mergeSort;
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,5 +1,19 @@
 const mergeSort = require('../src/index');
 
 describe('MergeSort', () => {
+  test('is a function', () => {
+    expect(typeof mergeSort).toBe('function');
+  });
 
+  test('returns an array', () => {
+    expect(Array.isArray(mergeSort())).toBe(true);
+  });
+
+  test('sorts an array', () => {
+    const unsortedNums = [5, 2, 1, 9, 4, 10, 7];
+    const sortedNums = unsortedNums.slice();
+    sortedNums.sort((a, b) => a - b);
+
+    expect(mergeSort(unsortedNums)).toEqual(sortedNums);
+  });
 });
